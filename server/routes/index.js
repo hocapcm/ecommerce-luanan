5088e672714ba90f4b1484efe2e6050d4407d28e
Blue <blue@MacBook-Air-cua-Nga.local>
2023-09-25 14:25:46 +0700

    #2 Thêm vài chức năng đăng ký, sửa thông báo lỗi cho gọn gàng

---
+++ server/routes/index.js
@@ -0,0 +1,16 @@
+const userRouter = require('./user');
+const {notFound,errHandler} = require('../middlewares/errHandler');
+
+
+
+const initRoutes = (app) => {
+    app.use('/api/user',userRouter);
+
+
+
+    app.use(notFound);
+    app.use(errHandler);
+
+}
+
+module.exports = initRoutes;
